2c0b3c2d73faec6ccb5ae83a0f6dddaabe0eaf35
K M Rejowan Ahmmed <ahmmedrejowan@gmail.com>
2021-05-16 21:57:03 +0600

    Snack Test

--- app/src/main/java/com/rejowan/cutesnackbarexample/MainActivity.java
+++ app/src/main/java/com/rejowan/cutesnackbarexample/MainActivity.java
@@ -1,8 +1,12 @@
 package com.rejowan.cutesnackbarexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Git all working fine
+
+        ConstraintLayout constraintLayout = findViewById(R.id.mLayout);
+
+        Snackbar.make(constraintLayout,"Hello", Snackbar.LENGTH_SHORT).show();
 
    }
 }
